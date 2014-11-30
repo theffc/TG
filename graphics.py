c2c24bbd4a72aba899afc27dd9f2548367c8505c
theffc <fred_ffc@hotmail.com>
2014-11-30 18:52:37 -0200

    rects everywhere

--- graphics.py
+++ graphics.py
@@ -1,48 +1,54 @@
 import pygame
+from tg import *
 
 
-FPS=30
+
+FPS=50
 quitGame=False
 
 pygame.init()
 vidInfo = pygame.display.Info()
+
 WIDTH = vidInfo.current_w
 HEIGHT = vidInfo.current_h
 RESOLUTION = (WIDTH, HEIGHT)
+RECT_SIZE = WIDTH * 0.1
 
 SCREEN = pygame.display.set_mode(RESOLUTION)#, pygame.FULLSCREEN)
 
-pygame.event.set_blocked([pygame.USEREVENT, pygame.VIDEOEXPOSE, pygame.MOUSEMOTION, pygame.ACTIVEEVENT, pygame.KEYDOWN, pygame.KEYUP, pygame.VIDEORESIZE, pygame.JOYAXISMOTION, pygame.JOYBALLMOTION, pygame.JOYHATMOTION, pygame.JOYBUTTONUP, pygame.JOYBUTTONDOWN])
+pygame.event.set_blocked([pygame.USEREVENT, pygame.VIDEOEXPOSE, pygame.MOUSEMOTION, pygame.ACTIVEEVENT, pygame.KEYDOWN, pygame.KEYUP, pygame.VIDEORESIZE, pygame.JOYAXISMOTION, pygame.JOYBALLMOTION, pygame.JOYHATMOTION, pygame.JOYBUTTONUP, pygame.JOYBUTTONDOWN, pygame.MOUSEBUTTONUP])
 
 clock = pygame.time.Clock()
+
 
 
 clique=0
 disclique=0
 while not quitGame:
 
-	events = pygame.event.get([pygame.QUIT, pygame.MOUSEBUTTONUP, pygame.MOUSEBUTTONDOWN])
+	events = pygame.event.get()
 	for event in events:
 		if event.type == pygame.QUIT:
 			quitGame=True
 			break
 		elif event.type == pygame.MOUSEBUTTONDOWN :
 			clique+=1
-			clock.tick_busy_loop(FPS/5)
+			clock.tick_busy_loop(FPS*0.1)
 			if pygame.event.get(pygame.MOUSEBUTTONDOWN):
-				##graph.newV(pygame.mouse.get_pos())
+				Rect = pygame.Rect(event.pos, (RECT_SIZE, RECT_SIZE))
+				Grafo.newV(Rect)
 				pygame.Surface.fill(SCREEN,(0,255,0))
+				Grafo.mostrarV()
 			else:
 				pygame.Surface.fill(SCREEN,(255,0,0))
 		elif event.type == pygame.MOUSEBUTTONUP:
 			disclique-=1
 
 	pygame.display.update()
-	print clique
+	
 	clique=0
 	disclique=0
-	##clock.tick(FPS)
-	clock.tick_busy_loop(FPS)
+	clock.tick(FPS)
 
 pygame.quit()
 
--- tg.py
+++ tg.py
@@ -1,4 +1,5 @@
-		
+from __future__ import print_function		
+
 class Grafo(object):
 	"""docstring for Grafo"""
 
@@ -7,25 +8,38 @@ class Grafo(object):
 	totalVertices=0
 	totalArestas=0
 		
-	def newV(xy):
-		v= Vertice(xy)
+	@staticmethod
+	def newV(Rect):
+		v= Vertice(Rect)
 		Grafo.vertices.append(v)
 		Grafo.totalVertices+=1
 
 	def newA():
 		pass
 
+	@staticmethod
+	def mostrarV():
+		print('[')	
+		for x in Grafo.vertices:
+			x.mostrar()
+		print(']')
+
+
 class Vertice(object):
 
 	vid=1
 
 	"""docstring for Vertice"""
-	def __init__(self, xy):
+	def __init__(self, Rect):
 		super(Vertice, self).__init__()
-		self.xy = xy
-		
-		self.id = self.vid
-		self.vid+=1
+		self.Rect = Rect
+		self.center = Rect.center
+		self.id = Vertice.vid
+		Vertice.vid = Vertice.vid + 1
+
+	def mostrar(self):
+		#print ( str(self.id)+ ' - ' + str(self.Rect) , ',', '')
+		print(self.id, self.center, sep=' - ')
 		
 		
 
@@ -37,5 +51,3 @@ class Aresta(object):
 		self.vertices = vertices
 		
 
-
-Grafo.newV(2)
