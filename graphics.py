ea7ce70af5c6b5312b5691c32ad32d3535b87991
theffc <fred_ffc@hotmail.com>
2014-11-30 14:01:27 -0200

    double clcik
    finally its supporting a double click

--- graphics.py
+++ graphics.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 FPS=30
 quitGame=False
 
@@ -9,39 +10,39 @@ WIDTH = vidInfo.current_w
 HEIGHT = vidInfo.current_h
 RESOLUTION = (WIDTH, HEIGHT)
 
-SCREEN = pygame.display.set_mode(RESOLUTION, pygame.FULLSCREEN)
+SCREEN = pygame.display.set_mode(RESOLUTION)#, pygame.FULLSCREEN)
 
-pygame.event.set_blocked([pygame.JOYAXISMOTION, pygame.JOYBALLMOTION, pygame.JOYHATMOTION, pygame.JOYBUTTONUP, pygame.JOYBUTTONDOWN])
+pygame.event.set_blocked([pygame.USEREVENT, pygame.VIDEOEXPOSE, pygame.MOUSEMOTION, pygame.ACTIVEEVENT, pygame.KEYDOWN, pygame.KEYUP, pygame.VIDEORESIZE, pygame.JOYAXISMOTION, pygame.JOYBALLMOTION, pygame.JOYHATMOTION, pygame.JOYBUTTONUP, pygame.JOYBUTTONDOWN])
 
 clock = pygame.time.Clock()
 
 
-click=0
-disclick=0
+clique=0
+disclique=0
 while not quitGame:
 
 	events = pygame.event.get([pygame.QUIT, pygame.MOUSEBUTTONUP, pygame.MOUSEBUTTONDOWN])
-	for x in events:
-		if x.type == pygame.QUIT:
+	for event in events:
+		if event.type == pygame.QUIT:
 			quitGame=True
 			break
-		elif x.type == pygame.MOUSEBUTTONDOWN :
-			click+=1
-			print x
-			pygame.Surface.fill(SCREEN,(255,0,0))
-			continue
-			if click>=2 and clock.tick()<FPS:
+		elif event.type == pygame.MOUSEBUTTONDOWN :
+			clique+=1
+			clock.tick_busy_loop(FPS/5)
+			if pygame.event.get(pygame.MOUSEBUTTONDOWN):
 				##graph.newV(pygame.mouse.get_pos())
-				pygame.Surface.fill(SCREEN, (0,255,0))
-				click=0
-		elif x.type == pygame.MOUSEBUTTONUP:
-			disclick-=1
+				pygame.Surface.fill(SCREEN,(0,255,0))
+			else:
+				pygame.Surface.fill(SCREEN,(255,0,0))
+		elif event.type == pygame.MOUSEBUTTONUP:
+			disclique-=1
 
 	pygame.display.update()
-
-	click=0
-	disclick=0
-	clock.tick(FPS)
+	print clique
+	clique=0
+	disclique=0
+	##clock.tick(FPS)
+	clock.tick_busy_loop(FPS)
 
 pygame.quit()
 
