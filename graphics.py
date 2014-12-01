7b3490d8272e3d3c49c589f04d2b86ab9d75d4bb
theffc <fred_ffc@hotmail.com>
2014-12-01 16:16:08 -0200

    changing without testing

--- graphics.py
+++ graphics.py
@@ -16,7 +16,7 @@ RECT_SIZE = int(WIDTH * 0.02)
 
 SCREEN = pygame.display.set_mode(RESOLUTION)#, pygame.FULLSCREEN)
 
-pygame.event.set_blocked([pygame.USEREVENT, pygame.VIDEOEXPOSE, pygame.MOUSEMOTION, pygame.ACTIVEEVENT, pygame.KEYDOWN, pygame.KEYUP, pygame.VIDEORESIZE, pygame.JOYAXISMOTION, pygame.JOYBALLMOTION, pygame.JOYHATMOTION, pygame.JOYBUTTONUP, pygame.JOYBUTTONDOWN, pygame.MOUSEBUTTONUP])
+pygame.event.set_blocked([pygame.USEREVENT, pygame.VIDEOEXPOSE, pygame.MOUSEMOTION, pygame.ACTIVEEVENT, pygame.KEYDOWN, pygame.KEYUP, pygame.VIDEORESIZE, pygame.JOYAXISMOTION, pygame.JOYBALLMOTION, pygame.JOYHATMOTION, pygame.JOYBUTTONUP, pygame.JOYBUTTONDOWN])
 
 clock = pygame.time.Clock()
 pygame.mouse.set_visible(1)
@@ -29,23 +29,42 @@ while not quitGame:
 
 	events = pygame.event.get()
 	for event in events:
+
 		if event.type == pygame.QUIT:
 			quitGame=True
 			break
-		elif event.type == pygame.MOUSEBUTTONDOWN :
-			clique+=1
+
+		elif event.type == pygame.MOUSEBUTTONDOWN: #clicou
+			local = Grafo.verificarClique(event.pos)
 			clock.tick_busy_loop(FPS*0.1)
-			if pygame.event.get(pygame.MOUSEBUTTONDOWN) and not Grafo.colidiuXY(event.pos):
-				# Rect = pygame.Rect(event.pos[0]-RECT_SIZE/2, event.pos[1]-RECT_SIZE/2, RECT_SIZE, RECT_SIZE)
+			if pygame.event.get(pygame.MOUSEBUTTONDOWN) and not isinstance(local, Vertice): # doubleclick
 				Rect=pygame.draw.circle(SCREEN,(0,0,255),event.pos,RECT_SIZE, RECT_SIZE/10)
 				Grafo.newV(Rect)
-				#pygame.Surface.fill(SCREEN,(0,255,0))
 				Grafo.mostrarV()
-			else:
-				#pygame.Surface.fill(SCREEN,(255,0,0))
-				pass
-		elif event.type == pygame.MOUSEBUTTONUP:
-			disclique-=1
+				continue
+		
+			if not local: # clicou no vazio
+				continue
+
+			local.bSelecionado=True
+			if pygame.mouse.get_pressed()[0]: # botao esquerdo do mouse continua pressionado
+				disclique = pygame.event.get(MOUSEBUTTONUP)
+				while not disclique:
+					pygame.event.wait()
+					disclique = pygame.event.get(MOUSEBUTTONUP)
+					
+				local2 = verificarClique(disclique.pos)
+				if isinstance(local2, Vertice):
+					# criar uma nova aresta
+					pass
+
+				else:
+					# Mover o rect do vertice
+					pass
+
+
+
+		
 
 	pygame.display.update()
 	
--- tg.py
+++ tg.py
@@ -3,54 +3,83 @@ from __future__ import print_function
 class Grafo(object):
 	"""docstring for Grafo"""
 
-	vertices=[]
-	arestas=[]
-	totalVertices=0
-	totalArestas=0
+	lVertices=[]
+	lArestas=[]
+	iTotalVertices=0
+	iTotalArestas=0
 		
 	@staticmethod
 	def newV(Rect):
 		v= Vertice(Rect)
-		Grafo.vertices.append(v)
-		Grafo.totalVertices+=1
+		Grafo.lVertices.append(v)
+		Grafo.iTotalVertices+=1
 
 	@staticmethod
 	def mostrarV():
 		print('[')	
-		for x in Grafo.vertices:
+		for x in Grafo.lVertices:
 			x.mostrar()
 		print(']')
 
 	@staticmethod
-	def colidiuXY(ponto):
-		for vertice in Grafo.vertices:
+	def verticeClique(ponto):
+		for vertice in Grafo.lVertices:
 			if vertice.Rect.collidepoint(ponto):
 				return True
 		return False
 
+	@staticmethod
+	def verificarClique(ponto):
+		for vertice in Grafo.lVertices:
+			if vertice.Rect.collidepoint(ponto):
+				return vertice
+
+		for aresta in Grafo.lArestas:
+			if aresta.Rect.collidepoint(ponto):
+				return aresta
+
+		return None
+
+
+	@staticmethod
+	def newA(v1, v2):
+		if v1.iID in v2.setAdjs or v2.iID in v1.setAdjs
+			return False
+
+		Rect = v1.Rect.union(v2.Rect)
+		aresta = [Rect,v1,v2]
+		lArestas.append(aresta)
+		Grafo.iTotalArestas+=1
+		v1.setAdjs.add(v2.iID)
+		v2.setAdjs.add(v1.iID)
+		return True
+
+
 class Vertice(object):
 
-	vid=1
+	iVid=1
 
 	"""docstring for Vertice"""
 	def __init__(self, Rect):
 		super(Vertice, self).__init__()
 		self.Rect = Rect
-		self.center = Rect.center
-		self.id = Vertice.vid
-		Vertice.vid += 1
+		self.tCenter = Rect.center
+		self.iID = Vertice.iVid
+		Vertice.iVid += 1
+		self.bSelecionado=False
+		self.setAdjs=set().add(Vertice.iVid)
 
 	def mostrar(self):
-		#print ( str(self.id)+ ' - ' + str(self.Rect) , ',', '')
-		print(self.id, self.center, sep=' - ')
+		#print ( str(self.iID)+ ' - ' + str(self.Rect) , ',', '')
+		print(self.iID, self.tCenter, sep=' - ')
 		
 		
 
 class Aresta(object):
 
 	"""docstring for Aresta"""
-	def __init__(self, vertices):
+	def __init__(self, lVertices):
 		super(Aresta, self).__init__()
-		self.vertices = vertices
+		self.lVertices = lVertices
 		
 
