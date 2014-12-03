883b32acc07b786c733727a4a561f2471c101d37
theffc <fred_ffc@hotmail.com>
2014-12-02 22:01:18 -0200

    changing without testing

--- graphics.py
+++ graphics.py
@@ -5,6 +5,9 @@ from tg import *
 
 FPS=50
 quitGame=False
+BLUE = (0,100,255)
+PINK = (255,0,255)
+
 
 pygame.init()
 vidInfo = pygame.display.Info()
@@ -13,6 +16,7 @@ WIDTH = vidInfo.current_w
 HEIGHT = vidInfo.current_h
 RESOLUTION = (WIDTH, HEIGHT)
 RECT_SIZE = int(WIDTH * 0.02)
+LINE = int(WIDTH * 0.005)
 
 SCREEN = pygame.display.set_mode(RESOLUTION)#, pygame.FULLSCREEN)
 
@@ -33,54 +37,64 @@ while not quitGame:
 			quitGame=True
 			break
 
-		elif event.type == pygame.MOUSEBUTTONDOWN: #clicou
+		#1 clicou
+		elif event.type == pygame.MOUSEBUTTONDOWN: 
 			local = Grafo.verificarClique(event.pos)
-			Grafo.selecionado = local
 			print local
 			clock.tick_busy_loop(FPS*0.1)
 			localEhUmVertice = isinstance(local, Vertice)
 
-			if pygame.event.get(pygame.MOUSEBUTTONDOWN) and not localEhUmVertice: # doubleclick
-				Rect=pygame.draw.circle(SCREEN,(0,100,255),event.pos,RECT_SIZE, RECT_SIZE/10)
+			#2 doubleclick
+			if pygame.event.get(pygame.MOUSEBUTTONDOWN) and not localEhUmVertice: 
+				Rect=pygame.draw.circle(SCREEN,BLUE,event.pos,RECT_SIZE, RECT_SIZE/10)
 				Grafo.newV(Rect)
 				Grafo.mostrarV()
 				continue
 		
-			elif not local: # clicou no vazio
+			#2 clicou no vazio
+			elif not local:
 				continue
 
-			elif localEhUmVertice and pygame.mouse.get_pressed()[0] : # botao esquerdo do mouse continua pressionado
+
+			#2 botao esquerdo do mouse continua pressionado
+			elif localEhUmVertice and pygame.mouse.get_pressed()[0] : 
+				
+				#3 pegar o disclique
 				disclique = pygame.event.get(pygame.MOUSEBUTTONUP)
 				while not disclique:
 					pygame.event.set_allowed(None)
 					pygame.event.set_allowed(pygame.MOUSEBUTTONUP)
 					disclique=pygame.event.wait()
 					print disclique
-					
 				local2 = Grafo.verificarClique(disclique.pos)
 				print local2
+
+				#3 desclicou num vertice para criar uma aresta
 				if isinstance(local2, Vertice):
 					aresta = Grafo.newA(local, local2)
 					print Grafo.iTotalArestas
-					pygame.draw.line(SCREEN, (255,0,255), local.tCenter, local2.tCenter )	# desclicou num vertice para criar uma aresta
-					
+					pygame.draw.line(SCREEN, PINK, local.Rect.center, local2.Rect.center )
+				
+				#3 desclicou no vazio-> Mover o rect do vertice
 				else:
-					# Mover o rect do vertice
 					SCREEN.fill((0,0,0), local.Rect)
-					local.Rect = pygame.draw.circle(SCREEN,(0,100,255),disclique.pos,RECT_SIZE, RECT_SIZE/10)
-					local.tCenter = local.Rect.center
+					local.Rect = pygame.draw.circle(SCREEN, BLUE,disclique.pos,RECT_SIZE, LINE)
 					local.iMudouPos = len(local.setAdjs)
+					local.bMudouPos = True
 
-
-			elif True: # verificar se o usuario apertou o delete
+			#2 usuario apertou o delete
+			elif True: 
 				pass
 
+		#1 pygame pega apenas eventos interesssantes
 		pygame.event.set_allowed([pygame.QUIT, pygame.KEYDOWN, pygame.MOUSEBUTTONUP, pygame.MOUSEBUTTONDOWN])
 
+	#for end
 	
 	pygame.display.update()
 	clock.tick(FPS)
 
+#while end
 
 pygame.quit()
 
--- tg.py
+++ tg.py
@@ -7,7 +7,8 @@ class Grafo(object):
 	lArestas=[]
 	iTotalVertices=0
 	iTotalArestas=0
-	selecionado=None
+	selectedV=None
+	selectedA=None
 		
 	@staticmethod
 	def newV(Rect):
@@ -32,34 +33,38 @@ class Grafo(object):
 
 	@staticmethod
 	def verificarClique(ponto):
+
 		for vertice in Grafo.lVertices:
 			if vertice.Rect.collidepoint(ponto):
+				Grafo.selectedV = vertice
 				return vertice
 
 		for aresta in Grafo.lArestas:
-			if aresta[1].iMudouPos or aresta[2].iMudouPos:
-				aresta[0] = aresta[1].Rect.union(aresta[2].Rect)
-				if not aresta[1]:
-					aresta[1].iMudouPos -= 1
+			
+			# um dos vertices foi deslocado -> criar novo rect pra aresta
+			if aresta.t[0].iMudouPos or aresta.t[1].iMudouPos:
+				aresta.Rect = aresta.t[0].Rect.union(aresta.t[1].Rect)
+				if not aresta.t[0]:
+					aresta.t[0].iMudouPos -= 1
 				else:
-					aresta[2].iMudouPos -= 1
-				
-
-			if aresta[0].collidepoint(ponto):
+					aresta.t[1].iMudouPos -= 1
+			
+			if aresta.Rect.collidepoint(ponto):
+				Grafo.selectedA=aresta
 				return aresta
 
+		Grafo.selectedA = None
+		Grafo.selectedV = None
 		return None
 
 
 	@staticmethod
 	def newA(v1, v2):
-		
 
+		# ja existe essa aresta
 		if v1.iID in v2.setAdjs:
 			return None
 
-		Rect = v1.Rect.union(v2.Rect)
-		aresta = [Rect, v1, v2]
 		Grafo.lArestas.append(aresta)
 		Grafo.iTotalArestas+=1
 		v1.setAdjs.add(v2.iID)
@@ -67,35 +72,55 @@ class Grafo(object):
 		return aresta
 
 	@staticmethod
-	def desenhar():
-		pass
+	def desenhar(Surf):
+
+		if Grafo.selectedV:
+			Grafo.selectedV.Rect = pygame.draw.circle(Surf,BLUE,Grafo.selectedV.Rect.center,RECT_SIZE, LINE)
+			Grafo.selectedV.bMudouPos = False
+
+		if Grafo.selectedA:
+			Grafo.selectedA.Rect = pygame.draw.line(Surf,BLUE,Grafo.selectedA.Rect.center,RECT_SIZE, LINE)
+			Grafo.selectedA.bMudouPos=False
+
+		for v in Grafo.lVertices:
+			if v.bMudouPos:
+				v.Rect = pygame.draw.circle(Surf,BLUE,v.Rect.center,RECT_SIZE, LINE)
+				v.bMudouPos = False
+
+		for a in Grafo.lArestas:
+			if a.bMudouPos:
+				a.Rect = pygame.draw.line(Surf, PINK, a.t[0].Rect.center, a.t[1].center, LINE)
+				a.bMudouPos = False
 
 class Vertice(object):
+	"""docstring for Vertice"""
 
 	iVid=0
 
-	"""docstring for Vertice"""
-	def __init__(self, Rect):
+		def __init__(self, Rect):
 		super(Vertice, self).__init__()
 		self.Rect = Rect
-		self.tCenter = Rect.center
 		self.iID = int( Vertice.iVid +1 )
 		Vertice.iVid += 1
 		self.setAdjs=set()
 		self.setAdjs.add(self.iID)
 		self.iMudouPos = 0
+		self.bMudouPos = True
 
 	def mostrar(self):
 		#print ( str(self.iID)+ ' - ' + str(self.Rect) , ',', '')
-		print(self.iID, self.tCenter, sep=' - ')
+		print(self.iID, self.Rect.center, sep=' - ')
 		
 		
 
 class Aresta(object):
-
 	"""docstring for Aresta"""
-	def __init__(self, lVertices):
+
+	def __init__(self, v1, v2):
 		super(Aresta, self).__init__()
+		self.Rect = v1.Rect.union(v2.Rect)
+		self.t = (v1, v2)
+		self.bMudouPos = True
 		
 		
 
